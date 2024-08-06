fbae843ff2abe6ab1fb1d4d96d510fc31a9521a5
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-06 14:46:06 +0530

    delete api in frontend part

--- frontend/src/components/utils/ApiFunctions.js
+++ frontend/src/components/utils/ApiFunctions.js
@@ -43,3 +43,16 @@ export async function getAllRooms() {
     }
     
 }
+
+
+// This function deletes a room by using room Id
+export async function deleteRoom(roomId) {
+
+    try{
+        const result = await api.delete(`rooms/delete/room/${roomId}`)
+        return result.data
+    }catch(error){
+        throw new Error(`Error deleting room ${error.message}`)
+    }
+    
+}
