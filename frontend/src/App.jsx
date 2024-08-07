0f865402171307ad3dda8508e1a4db98fdef6520
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-07 21:50:55 +0530

    added NavBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import ExistingRooms from './components/room/ExistingRooms'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import EditRoom from './components/room/EditRoom'
 import Home from './components/home/Home'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
 
 function App() {
   
@@ -15,6 +17,7 @@ function App() {
     <>
     <main>
       <Router>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/edit-room/:roomId' element={<EditRoom/>}/>
@@ -22,6 +25,7 @@ function App() {
           <Route path='/add-room' element={<AddRoom/>}/>
         </Routes>
       </Router>
+      <Footer/>
     </main>
     </>
   )
