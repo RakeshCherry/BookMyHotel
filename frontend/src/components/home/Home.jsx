feb2d5e10e6685a78f3694217aad8d7e1cd7bddc
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-09 14:54:47 +0530

    added RoomCarousel component

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import MainHeader from "../layout/MainHeader"
 import HotelServices from '../common/HotelServices'
 import Parallax from '../common/Parallax'
+import RoomCarousel from '../common/RoomCarousel'
 
 const Home = () => {
   return (
     <section >
         <MainHeader/>
         <section className='container'>
+          <RoomCarousel/>
           <Parallax/>
+          <RoomCarousel/>
           <HotelServices/>
           <Parallax/>
+          <RoomCarousel/> 
         </section>
     </section>
   )
