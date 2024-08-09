23bb68d90ec976f7d39be3784eb5bd79d00ff8fd
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-09 13:31:04 +0530

    added RoomListing component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import EditRoom from './components/room/EditRoom'
 import Home from './components/home/Home'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
 
 function App() {
   
@@ -23,6 +24,7 @@ function App() {
           <Route path='/edit-room/:roomId' element={<EditRoom/>}/>
           <Route path='/existing-rooms' element={<ExistingRooms/>}/>
           <Route path='/add-room' element={<AddRoom/>}/>
+          <Route path='/browse-all-rooms' element={<RoomListing/>}/>
         </Routes>
       </Router>
       <Footer/>
