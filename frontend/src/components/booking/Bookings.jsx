8823ad48468c2d583268353537eeceae6301cc28
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-19 12:18:02 +0530

    started coding  Booking components

--- frontend/src/components/booking/Bookings.jsx
+++ frontend/src/components/booking/Bookings.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Bookings = () => {
+  const [bookingInfo, setBookingInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAllBookings()
+        .then((data) => {
+          setBookingInfo(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    });
+  }, []);
   return <div></div>;
 };
 
