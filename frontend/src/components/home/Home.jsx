f572bea6c3db9afb5880b101be6ad6c64494ccd2
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-26 15:37:29 +0530

    .

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import MainHeader from "../layout/MainHeader"
-import HotelServices from '../common/HotelServices'
-import Parallax from '../common/Parallax'
-import RoomCarousel from '../common/RoomCarousel'
+import React from "react";
+import MainHeader from "../layout/MainHeader";
+import HotelServices from "../common/HotelServices";
+import Parallax from "../common/Parallax";
+import RoomCarousel from "../common/RoomCarousel";
+import RoomSearch from "../common/RoomSearch";
 
 const Home = () => {
   return (
-    <section >
-        <MainHeader/>
-        <section className='container'>
-          <RoomCarousel/>
-          <Parallax/>
-          <RoomCarousel/>
-          <HotelServices/>
-          <Parallax/>
-          <RoomCarousel/> 
-        </section>
+    <section>
+      <MainHeader />
+      <section className="container">
+        <RoomSearch />
+        <RoomCarousel />
+        <Parallax />
+        <RoomCarousel />
+        <HotelServices />
+        <Parallax />
+        <RoomCarousel />
+      </section>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
