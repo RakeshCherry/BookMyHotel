2a72617bda5e4acf36e3c7ed742d5f6f785d8c8e
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-08-28 23:25:59 +0530

    creating UserSerivice class

---
+++ backend/src/main/java/com/bookmyhotel/BookMyHotel/Repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.bookmyhotel.BookMyHotel.Repository;
+
+import com.bookmyhotel.BookMyHotel.Model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String role);
+}
--- backend/src/main/java/com/bookmyhotel/BookMyHotel/Service/UserService.java
+++ backend/src/main/java/com/bookmyhotel/BookMyHotel/Service/UserService.java
@@ -1,17 +1,29 @@
 package com.bookmyhotel.BookMyHotel.Service;
 
+import com.bookmyhotel.BookMyHotel.Model.Role;
 import com.bookmyhotel.BookMyHotel.Model.User;
+import com.bookmyhotel.BookMyHotel.Repository.RoleRepository;
 import com.bookmyhotel.BookMyHotel.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 
+import java.util.Collection;
 import java.util.List;
 
+@RequiredArgsConstructor
 public class UserService implements IUserService{
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final passwordEndoder passwordEndoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public User registerUser(User user) {
-        if (userRepository != null)
-        return null;
+        if (userRepository.existByEmail(user.getEmail())){
+            throw new UserAlreadyExistsEception(user.getEmail() + "already exists");
+        }
+        user.setPassword(passwordEndoder.endcode(user.getPassword()));
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        user.setRoles(Collection.singletonList(userRole));
+        return userRepository.save(user);
     }
 
     @Override
