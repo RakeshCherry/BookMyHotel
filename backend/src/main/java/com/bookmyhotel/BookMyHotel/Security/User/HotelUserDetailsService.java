2058f08a03d8616da45e71dfaafd8764c1f1c0f6
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-09-05 21:11:14 +0530

    HotelUserDetailsService

---
+++ backend/src/main/java/com/bookmyhotel/BookMyHotel/Security/User/HotelUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.bookmyhotel.BookMyHotel.Security.User;
+
+import com.bookmyhotel.BookMyHotel.Model.User;
+import com.bookmyhotel.BookMyHotel.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
