49ddbe5f0fad40cfc2e152e0d50b1695bbc49c8f
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-09-10 12:18:24 +0530

    adding jwt  security

--- backend/src/main/java/com/bookmyhotel/BookMyHotel/Security/jwt/JwtUtils.java
+++ backend/src/main/java/com/bookmyhotel/BookMyHotel/Security/jwt/JwtUtils.java
@@ -1,4 +1,11 @@
 package com.bookmyhotel.BookMyHotel.Security.jwt;
 
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
 public class JwtUtils {
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(JwtUtils.class);
+    private String jwtSecrets;
+    private int jwtExpirationTime;
 }
